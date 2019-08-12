237751d1aeef243e402e81c7364fc56b55be2a50
Holonium <holonium2.0@gmail.com>
2019-08-11 17:29:20 -0700

    Edited Logic

--- validation.c
+++ validation.c
@@ -8,24 +8,18 @@ int validateTeacher(int teachers[6], int range){
 	int valid[6];
 	int valid1[6];
 	int valid2[6];
-	int i;
-	int v = 1;
+	int i;;
 	for(i = 0; i < 6; i++){
-		if(teachers[i] <= range){
+		if(teachers[i] <= range && teachers[i] > 0){
 			valid1[i] = 1;
 		} else {
 			valid1[i] = 0;
 		}
-		if(teachers[i] > 0){
-			valid2[i] = 1;
-		} else {
-			valid2[i] = 0;
-		}
 	}
 	for(i = 0; i < 6; i++){
 		if(valid1[i] == 0 || valid2[i] == 0){
-			v = 0;
+			return 0;
 		}
 	}
-	return v;
+	return 1;
 }
