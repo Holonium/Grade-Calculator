d27934a70df4e8d5e53d3b80f1f75e39ab644504
Holonium <kellenmwest@gmail.com>
2019-10-20 15:22:38 +0000

    Base

--- calc.h
+++ calc.h
@@ -9,4 +9,8 @@ double rounding_select(int teacher);
 double finalCalc(double q1grade, double q2grade, double q1weight, double q2weight, double fweight, double target);
 double roundCalc(double gradeRaw, double roundingFactor);
 
+int verify_classes(int periods);
+int verify_teacher(int teacher);
+int verify_grades
+
 #endif
---
+++ validation.c
@@ -0,0 +1,7 @@
+//This file handles the verification of inputs and error handling.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "calc.h"
+
+
