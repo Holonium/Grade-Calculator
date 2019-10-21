f1527b7a2fa48d8a12925661eb26ece10956b3f4
Holonium <kellenmwest@gmail.com>
2019-10-21 16:48:55 +0000

    Added input validation

--- Makefile
+++ Makefile
@@ -1,14 +1,16 @@
 LD_LIBRARY_PATH=.
 
-grades : grades.o teachers.o calc.o calc.h
-	gcc -o grades grades.o teachers.o calc.o
+grades : grades.o teachers.o calc.o validation.o calc.h
+	gcc -o grades grades.o teachers.o calc.o validation.o
 
-grades.o : grades.c teachers.o calc.o calc.h
+grades.o : grades.c calc.h
 	gcc -c -o grades.o grades.c
 teachers.o : teachers.c calc.h
 	gcc -c -o teachers.o teachers.c
 calc.o : calc.c calc.h
 	gcc -c -o calc.o calc.c
+validation.o : validation.c calc.h
+	gcc -c -o validation.o validation.c
 
 clean :
-	rm grades grades.o teachers.o calc.o
+	rm grades grades.o teachers.o calc.o validation.o
--- validation.c
+++ validation.c
@@ -5,8 +5,35 @@
 #include "calc.h"
 
 int verify_classes(char *input){
-	
+	int verify = atoi(input);
+	if(verify == 0){
+		return 1;
+	} else if(verify > 8){
+		return 1;
+	} else if(verify < 1){
+		return 1;
+	}
+	return 0;
 }
 
-int verify_teacher(char *input);
-int verify_grades(char *input);
+int verify_teacher(char *input){
+	int verify = atoi(input);
+	if(verify == 0){
+		return 1;
+	} else if(verify > 13){
+		return 1;
+	} else if(verify < 1){
+		return 1;
+	}
+	return 0;
+}
+
+int verify_grades(char *input){
+	double verify = atof(input);
+	if(verify == 0){
+		return 1;
+	} else if(verify < 0){
+		return 1;
+	}
+	return 0;
+}
