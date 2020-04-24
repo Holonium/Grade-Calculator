3076a53330ab773e0f24d4dfe6eee3bea7f8f1b5
Holonium <holonium2.0@gmail.com>
2020-04-24 11:11:21 -0700

    Added teacher constants and built framework
    Added the constants for teachers and began building the base framework for the program.

--- src/grades.c
+++ src/grades.c
@@ -7,17 +7,44 @@ int main(){
 	char input[10];
 	int isValid;
 	int periods;
+	int years;
 
 	printf("Welcome to %s of the grade calculator.\n", version);
 	printf("WARNING: THIS IS AN ALPHA RELEASE, STABILITY IS NOT ENSURED!\n");
-	ERR1: printf("Please enter the number of periods that you wish to calculate grades for (1-8):");
+	ERR1: printf("Please enter how many years you have been in school for GPA calculations: ");
+	fgets(input, 10, stdin);
+	isValid = verify_classes(input);
+	if(isValid != 0){
+		printf("Enter a valid number of years! ");
+		goto ERR1;
+	} else {
+		years = atoi(input);
+	}
+	ERR2: printf("Please enter the number of periods that you wish to calculate grades for: ");
 	fgets(input, 10, stdin);
 	isValid = verify_classes(input);
 	if(isValid != 0){
 		printf("Enter a valid number of periods! ");
-		goto ERR1;
+		goto ERR2;
 	} else {
 		periods = atoi(input);
+	}
+	int periodYears[years];
+	for(int i = 0; i < years; i++){
+		int j = i + 1;
+		ERR3: printf("Please enter the number of periods for year %i: ",j);
+		fgets(input, 10, stdin);
+		isValid = verify_classes(input);
+		if(isValid != 0){
+			printf("Enter a valid number of periods! ");
+			goto ERR3;
+		} else {
+			periodYears[i] = atoi(input);
+		}
+		int k = years - 1;
+		if(i == k){
+			periodYears[i] = periods;
+		}
 	}
 	
 	int teachers[periods];
@@ -31,5 +58,19 @@ int main(){
 	double fgrades1[periods];
 	double percentages[periods][11];
 	double results[periods][11];
+	double credits[years][periods];
+	double semesterGrades[years][2][periods];
+	double gradePoints[years][periods];
+	int types[years][periods];
 	
+	int m = years - 1;
 	
+	for(int i = 0; i < m; i++){
+		for(int j = 0; j < periodYears[i]; j++){
+			int n = i + 1;
+			for(int k = 0; k < 2; k++){
+				int l = k + 1;
+				ERR4: printf("Please enter your grade for semester %i of year %i: ",l,n);
+				fgets(input, 10, stdin);
+				isValid = verify_grades(input);
+				
