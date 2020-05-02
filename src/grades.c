e497dda23a5588353a8b9bb4f3d57ec784c87b19
Holonium <holonium2.0@gmail.com>
2020-05-02 14:38:58 -0700

    Fixed multiple definition errors

--- src/grades.c
+++ src/grades.c
@@ -3,6 +3,26 @@
 #include "grades.h"
 #include "functions.h"
 
+extern const char version[] = "v1.0.0a1";
+extern const double percentagesRaw[] = {93,90,87,83,80,77,73,70,67,63,60};
+extern const char grades[] = {'A','A','B','B','B','C','C','C','D','D','D'};
+extern const char symbol[] = {'\0','-','+','\0','-','+','\0','-','+','\0','-'};
+extern const double gpa = 4.00;
+extern const char *teacher_list_charter = "1) Mrs. Baker\n2) Mr. Sabatke\n3) Ms. Bright\n4) Mr. Ginorio\n5) Mr. McCormack\n6) Mr. Gabrielsen\n7) Mr. Harrison\n8) Mr. Williams\n9) Mr. Hall\n10) Mrs. Vaughan\n11) Mrs. Ketchum\n12) Ms. Solsvik\n13) Mr. Rigg\n14) Other\nPlease enter the number of the teacher for period ";
+extern const char *class_types = "1) Standard\n2) Honors\n3) AP\nPlease enter the number of the class type for period ";
+
+extern const char *grade1 = "Please enter your grade for the ";
+extern const char *grade2 = " quarter of this semester: ";
+
+extern const char *guaranteed1 = "You are guaranteed to get at least an ";
+extern const char *guaranteed2 = "You are guaranteed to get at least a ";
+extern const char *guaranteed3 = " for the semester!";
+
+extern const char *out1 = "You need at least a ";
+extern const char *out2 = " to get a ";
+extern const char *out3 = " for this semester.";
+
+
 int main(){
 	printf("Welcome to %s of the grade calculator.\n", version);
 	printf("WARNING: THIS IS AN ALPHA RELEASE, STABILITY IS NOT ENSURED!\n");
--- src/grades.h
+++ src/grades.h
@@ -1,23 +1,22 @@
 #ifndef GRADES_H_
 #define GRADES_H_
 
-const char version[] = "v1.0.0a1";
-const double percentagesRaw[] = {93,90,87,83,80,77,73,70,67,63,60};
-const char grades[] = {'A','A','B','B','B','C','C','C','D','D','D'};
-const char symbol[] = {'\0','-','+','\0','-','+','\0','-','+','\0','-'};
-const double gpa = 4.00;
-const char *teacher_list_charter = "1) Mrs. Baker\n2) Mr. Sabatke\n3) Ms. Bright\n4) Mr. Ginorio\n5) Mr. McCormack\n6) Mr. Gabrielsen\n7) Mr. Harrison\n8) Mr. Williams\n9) Mr. Hall\n10) Mrs. Vaughan\n11) Mrs. Ketchum\n12) Ms. Solsvik\n13) Mr. Rigg\n14) Other\nPlease enter the number of the teacher for period ";
-const char *class_types = "1) Standard\n2) Honors\n3) AP\nPlease enter the number of the class type for period ";
+const char version[];
+const double percentagesRaw[];
+const char grades[];
+const char symbol[];
+const double gpa;
+const char *teacher_list_charter;
 
-char *grade1 = "Please enter your grade for the ";
-char *grade2 = " quarter of this semester: ";
+const char *grade1;
+const char *grade2;
 
-char *guaranteed1 = "You are guaranteed to get at least an ";
-char *guaranteed2 = "You are guaranteed to get at least a ";
-char *guaranteed3 = " for the semester!";
+const char *guaranteed1;
+const char *guaranteed2;
+const char *guaranteed3;
 
-char *out1 = "You need at least a ";
-char *out2 = " to get a ";
-char *out3 = " for this semester.";
+const char *out1;
+const char *out2;
+const char *out3;
 
 #endif
