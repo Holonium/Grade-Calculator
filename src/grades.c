2f0001ab3dd123946e067e7d3c81501a5e4fb2f1
Holonium <holonium2.0@gmail.com>
2020-05-05 11:42:52 -0700

    Finished framework

--- src/grades.c
+++ src/grades.c
@@ -35,8 +35,7 @@ int main(){
 
 	while(verify_YN(GPA) == 0){
 		printf("Do you wish to calculate your GPA? (Y/N) ");
-		fflush(stdin);
-		scanf("%c", GPA);
+		scanf(" %c", GPA);
 		if(verify_YN(GPA) != 0){
 			break;
 		}
@@ -46,8 +45,7 @@ int main(){
 		int years = 0;
 		while(verify_years(years) != 0){
 			printf("Please enter the number of years that you have completed (1-3): ");
-			fflush(stdin);
-			scanf("%i", &years);
+			scanf(" %d", &years);
 			if(verify_years(years) == 0){
 				break;
 			}
@@ -59,8 +57,7 @@ int main(){
 				int periods = 0;
 				while(verify_periods(periods) != 0){
 					printf("Please enter the number of periods for semester %i of year %i (1-8): ", j, year);
-					fflush(stdin);
-					scanf("%i", &periods);
+					scanf(" %d", &periods);
 					if(verify_periods(periods) == 0){
 						break;
 					}
@@ -74,8 +71,7 @@ int main(){
 					int period = k + 1;
 					while(verify_grade(grade) != 0){
 						printf("Please enter your grade for period %i of semester %i of year %i: ", period, j, year);
-						fflush(stdin);
-						scanf("%lf", &grade);
+						scanf(" %lf", &grade);
 						if(verify_grade(grade) == 0){
 							break;
 						}
@@ -83,8 +79,7 @@ int main(){
 					}
 					while(verify_type(type) != 0){
 						printf("%s%i of semester %i of year %i (1-3): ", class_types, period, j, year);
-						fflush(stdin);
-						scanf("%i", &type);
+						scanf(" %d", &type);
 						if(verify_type(type) == 0){
 							break;
 						}
@@ -92,8 +87,7 @@ int main(){
 					}
 					while(verify_credits(credits) != 0){
 						printf("Please enter the number of credits for period %i of semester %i of year %i: ", period, j, year);
-						fflush(stdin);
-						scanf("%lf", &credits);
+						scanf(" %lf", &credits);
 						if(verify_credits(credits) == 0){
 							break;
 						}
@@ -101,8 +95,7 @@ int main(){
 					}
 					while(verify_rounding(roundingTemp) != 0){
 						printf("Please enter the point at which the teacher rounds: ");
-						fflush(stdin);
-						scanf("%lf", &roundingTemp);
+						scanf(" %lf", &roundingTemp);
 						if(verify_rounding(roundingTemp) == 0){
 							break;
 						}
@@ -128,8 +121,7 @@ int main(){
 	memset(sem, '\0', 2);
 	while(verify_YN(sem) == 0){
 		printf("Are you currently in semester 2 of the year? (Y/N) ");
-		fflush(stdin);
-		scanf("%c", sem);
+		scanf(" %c", sem);
 		if(verify_YN(sem) != 0){
 			break;
 		}
@@ -139,8 +131,7 @@ int main(){
 		int periods = 0;
 		while(verify_periods(periods) != 0){
 			printf("Please enter the number of periods for semester 1 of the current year (1-8): ");
-			fflush(stdin);
-			scanf("%i", &periods);
+			scanf(" %d", &periods);
 			if(verify_periods(periods) == 0){
 				break;
 			}
@@ -154,8 +145,7 @@ int main(){
 			int period = i + 1;
 			while(verify_grade(grade) != 0){
 				printf("Please enter the graded for period %i of semester 1 of the current year: ", period);
-				fflush(stdin);
-				scanf("%lf", &grade);
+				scanf(" %lf", &grade);
 				if(verify_grade(grade) == 0){
 					break;
 				}
@@ -163,8 +153,7 @@ int main(){
 			}
 			while(verify_type(type) != 0){
 				printf("%s%i of semester 1 of the current year (1-3): ", class_types, period);
-				fflush(stdin);
-				scanf("%d", &type);
+				scanf(" %d", &type);
 				if(verify_type(type) == 0){
 					break;
 				}
@@ -172,8 +161,7 @@ int main(){
 			}
 			while(verify_credits(credits) != 0){
 				printf("Please enter the number of credits for period %i of semester 1 of the current year: ", period);
-				fflush(stdin);
-				scanf("%lf", &credits);
+				scanf(" %lf", &credits);
 				if(verify_credits(credits) == 0){
 					break;
 				}
@@ -181,8 +169,7 @@ int main(){
 			}
 			while(verify_rounding(roundingTemp) != 0){
 				printf("Please enter the point at which the teacher rounds: ");
-				fflush(stdin);
-				scanf("%lf", &roundingTemp);
+				scanf(" %lf", &roundingTemp);
 				if(verify_rounding(roundingTemp) == 0){
 					break;
 				}
@@ -202,8 +189,7 @@ int main(){
 	int periods = 0;
 	while(verify_periods(periods) != 0){
 		printf("Please enter the number of periods for the current semester (1-8): ");
-		fflush(stdin);
-		scanf("%i", &periods);
+		scanf(" %d", &periods);
 		if(verify_periods(periods) == 0){
 			break;
 		}
@@ -218,15 +204,17 @@ int main(){
 	double f1weight[8] = { 0 };
 	double f2weight[8] = { 0 };
 	double f2grades[8] = { 0 };
+	double credits[8] = { 0 };
+	int types[8] = { 0 };
 	double percentages[8][11];
+	double finalGrades[8][11];
 	char *finals = malloc(2 * sizeof(char));
 	memset(finals, '\0', 2);
 	for(int i = 0; i < periods; i++){
 		int period = i + 1;
 		while(verify_teacher(teachers[i]) != 0){
 			printf("%s%i: ",teacher_list_charter,period);
-			fflush(stdin);
-			scanf("%i", &teachers[i]);
+			scanf(" %d", &teachers[i]);
 			if(verify_teacher(teachers[i]) == 0){
 				break;
 			}
@@ -235,8 +223,7 @@ int main(){
 		if(teachers[i] == 14){
 			while(verify_weight(q1weight[i]) != 0){
 				printf("Please enter the weight of the first quarter as a decimal: ");
-				fflush(stdin);
-				scanf("%lf", &q1weight[i]);
+				scanf(" %lf", &q1weight[i]);
 				if(verify_weight(q1weight[i]) == 0){
 					break;
 				}
@@ -244,17 +231,116 @@ int main(){
 			}
 			while(verify_weight(q2weight[i]) != 0){
 				printf("Please enter the weight of the second quarter as a decimal: ");
-				fflush(stdin);
-				scanf("%lf", &q2weight[i]);
+				scanf(" %lf", &q2weight[i]);
 				if(verify_weight(q2weight[i]) == 0){
 					break;
 				}
 				printf("Enter a valid weight! ");
 			}
+			while(verify_weight(f1weight[i]) != 0){
+				printf("Please enter the weight of the final as a decimal: ");
+				scanf(" %lf", &f1weight[i]);
+				if(verify_weight(f1weight[i]) == 0){
+					break;
+				}
+				printf("Enter a valid weight! ");
+			}
+			double roundingTemp;
+			while(verify_rounding(roundingTemp) != 0){
+				printf("Please enter the point at which the teacher rounds: ");
+				scanf(" %lf", &roundingTemp);
+				if(verify_rounding(roundingTemp) == 0){
+					if(roundingTemp == 0){
+						rounding[i] = 0;
+					} else {
+						rounding[i] = 1 - roundingTemp;
+					}
+					break;
+				}
+				printf("Enter a valid rounding point! ");
+			}
 		} else {
-
+			q1weight[i] = q1weight_select(teachers[i]);
+			q2weight[i] = q2weight_select(teachers[i]);
+			f1weight[i] = fweight_select(teachers[i]);
+			rounding[i] = rounding_select(teachers[i]);
+			if(teachers[i] == 3){
+				f2weight[i] = fweight_select(teachers[i]);
+			}
+		}
+		while(verify_grade(q1grades[i]) != 0){
+			printf("Please enter your grade for the first quarter of period %i: ", period);
+			scanf(" %lf", &q1grades[i]);
+			if(verify_grade(q1grades[i]) == 0){
+				break;
+			}
+			printf("Enter a valid grade! ");
+		}
+		while(verify_grade(q2grades[i]) != 0){
+			printf("Please enter your grade for the second quarter of period %i: ", period);
+			scanf(" %lf", &q2grades[i]);
+			if(verify_grade(q2grades[i]) == 0){
+				break;
+			}
+			printf("Enter a valid grade! ");
+		}
+		if(teachers[i] == 3){
+			while(verify_grade(f2grades[i]) != 0){
+				printf("Please enter your grade for the first part of the final: ");
+				scanf(" %lf", &f2grades[i]);
+				if(verify_grade(f2grades[i]) == 0){
+					break;
+				}
+				printf("Enter a valid grade! ");
+			}
+		}
+		while(verify_credits(credits[i]) != 0){
+			printf("Please enter the number of credits this class is worth: ");
+			scanf(" %lf", &credits[i]);
+			if(verify_credits(credits[i]) == 0){
+				break;
+			}
+			printf("Enter a valid number of credits! ");
+		}
+		while(verify_type(types[i]) != 0){
+			printf("%s%i of this year (1-3): ", class_types, period);
+			scanf(" %d", &types[i]);
+			if(verify_type(types[i]) == 0){
+				break;
+			}
+			printf("Enter a valid type! ");
+		}
+		for(int j = 0; j < 11; j++){
+			percentages[i][j] = round_calc(percentagesRaw[j], rounding[i]);
+			if(teachers[i] == 3){
+				finalGrades[i][j] = final_calc_multiweight(q1grades[i], q2grades[i], f2grades[i], q1weight[i], q2weight[i], f2weight[i], f1weight[i], percentages[i][j]);
+			} else {
+				finalGrades[i][j] = final_calc(q1grades[i], q2grades[i], q1weight[i], q2weight[i], f1weight[i], percentages[i][j]);
+			}
 		}
 	}
+	for(int i = 0; i < periods; i++){
+		int period = i + 1;
+		for(int j = 0; j < 11; j++){
+			if(finalGrades[i][j] <= 0){
+				if(j <= 1){
+					printf("Period %d: %s%c%c%s\n",period,guaranteed1,grades[j],symbol[j],guaranteed3);
+				}
+				if(j > 1){
+					printf("Period %d: %s%c%c%s\n",period,guaranteed2,grades[j],symbol[j],guaranteed3);
+				}
+				totalGradePointsUnweighted += gpa_class(finalGrades[i][j], credits[i], rounding[i], 1);
+				totalGradePointsWeighted += gpa_class(finalGrades[i][j], credits[i], rounding[i], types[i]);
+				totalCredits += credits[i];
+				j = 11;
+			} else {
+				printf("Period %d: %s%.2f%%%s%c%c%s\n",period,out1,finalGrades[i][j],out2,grades[j],symbol[j],out3);
+			}
+		}
+	}
+	double GPAWeight = gpa_full(totalGradePointsWeighted, totalCredits);
+	double GPAUnweight = gpa_full(totalGradePointsUnweighted, totalCredits);
+	printf("Your minimum weighted GPA is: %.3f\nYour minimum unweighted GPA is: %.3f\n", GPAWeight, GPAUnweight);
 
 	free(GPA);
 	free(sem);
--- src/teachers.c
+++ src/teachers.c
@@ -2,58 +2,48 @@
 #include <stdlib.h>
 #include "functions.h"
 
-void q1weight_select(int teacher){
+double q1weight_select(int teacher){
 	if(teacher == 1 || teacher == 3 || teacher == 4 || teacher == 7 || teacher == 8 || teacher == 13){
-		double temp = 0.45;
-		q1temp = &temp;
+		return 0.45;
 	} else if(teacher == 2 || teacher == 6 || teacher == 9 || teacher == 10 || teacher == 12){
-		double temp = 0.4;
-		q1temp = &temp;
+		return 0.4;
 	} else if(teacher == 11 || teacher == 5){
-		double temp = 0;
-		q1temp = &temp;
-	}	
+		return 0;
+	}
+	return 1;
 }
 
-void q2weight_select(int teacher){
+double q2weight_select(int teacher){
 	if(teacher == 1 || teacher == 3 || teacher == 4 || teacher == 7 || teacher == 8 || teacher == 13){
-		double temp = 0.45;
-		q2temp = &temp;
+		return 0.45;
 	} else if(teacher == 2 || teacher == 5 || teacher == 6 || teacher == 9 || teacher == 10 || teacher == 12){
-		double temp = 0.8;
-		q2temp = &temp;
+		return 0.4;
 	} else if(teacher == 11){
-		double temp = 1;
-		q2temp = &temp;
+		return 1;
 	}
+	return 1;
 }
 
-void fweight_select(int teacher){
+double fweight_select(int teacher){
 	if(teacher == 1 || teacher == 4 || teacher == 7 || teacher == 8 || teacher == 13){
-		double temp = 0.1;
-		fwtemp = &temp;
+		return 0.1;
 	} else if(teacher == 2 || teacher == 5 || teacher == 6 || teacher == 9 || teacher == 10 || teacher == 12){
-		double temp = 0.2;
-		fwtemp = &temp;
+		return 0.2;
 	} else if(teacher == 11){
-		double temp = 0;
-		fwtemp = &temp;
+		return 0;
 	} else if(teacher == 3){
-		double temp = 0.05;
-		fwtemp = &temp;
-		f1wtemp = &temp;
+		return 0.05;
 	}
+	return 1;
 }
 
-void rounding_select(int teacher){
+double rounding_select(int teacher){
 	if(teacher == 1 || teacher == 2 || teacher == 7 || teacher == 8 || teacher == 11 || teacher == 13){
-		double temp = 0;
-		roundtemp = &temp;
+		return 0;
 	} else if(teacher == 5 || teacher == 12){
-		double temp = 0.2;
-		roundtemp = &temp;
+		return 0.2;
 	} else if(teacher == 3 || teacher == 4 || teacher == 6 || teacher == 9 || teacher == 10){
-		double temp = 0.5;
-		roundtemp = &temp;
+		return 0.5;
 	}
+	return 1;
 }
